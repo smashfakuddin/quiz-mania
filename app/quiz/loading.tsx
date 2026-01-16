c919fa94528ddfaeb30ec4a6913eda07f2058fb1
S.M. Ashfak Uddin <smashfakuddin.anthro.cou@gmail.com>
2026-01-16 20:13:49 +0600

    more skeleton loader added to the admin and quiz

--- app/quiz/loading.tsx
+++ app/quiz/loading.tsx
@@ -1,16 +1,20 @@
 import SkeletonLoader from "@/components/common/skeleton-loader";
+import { Card } from "@/components/ui/card";
 
 export default function Loading() {
   return (
-    <div className="h-screen py-10 space-y-3 px-4">
-      <SkeletonLoader className="h-60 w-full rounded-lg" />
-
+    <div className="h-screen py-10 space-y-6 px-4">
+      {/* Row 1 */}
       <div className="flex gap-5">
         <SkeletonLoader className="h-48 flex-1 rounded-lg" />
-        <SkeletonLoader className="h-48 flex-1 rounded-lg" />
-        <SkeletonLoader className="h-48 flex-1 rounded-lg" />
-        <SkeletonLoader className="h-48 flex-1 rounded-lg" />
+        <SkeletonLoader className="h-48 max-w-48 w-full rounded-lg" />
       </div>
+
+      {/* Row 2 */}
+
+      <SkeletonLoader className="h-48 w-full flex-1 rounded-lg" />
+
+      {/* Row 3 */}
       <div className="flex gap-5">
         <SkeletonLoader className="h-48 flex-1 rounded-lg" />
         <SkeletonLoader className="h-48 flex-1 rounded-lg" />
