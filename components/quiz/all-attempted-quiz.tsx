4f2346ee1baf0101cdb0d761cd9a0b7b9c7444fa
S.M. Ashfak Uddin <smashfakuddin.anthro.cou@gmail.com>
2026-01-21 12:21:26 +0600

    all recent quiz added with api

--- components/quiz/all-attempted-quiz.tsx
+++ components/quiz/all-attempted-quiz.tsx
@@ -1,77 +1,32 @@
+import { getPreviousAttemptedQuiz } from "@/lib/mongo-query";
 import { Button } from "../ui/button";
 import { Card } from "../ui/card";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default function AllAttempTedQuiz() {
+export default async function AllAttempTedQuiz() {
+  const session = await auth();
+  if (!session?.user) {
+    redirect("/");
+  }
+  const quizzes = await getPreviousAttemptedQuiz(session?.user?.id);
+
   return (
     <div className=" grid grid-cols-1 md:grid-cols-3 gap-5">
-      <Card className=" hover:scale-[102%] transition-all duration-300">
-        <div className="flex">
-          <div>
-            <img src="/image/404-computer.svg" alt="" className="w-52" />
+      {quizzes.map((quiz) => (
+        <Card className=" hover:scale-[102%] transition-all duration-300">
+          <div className="flex">
+            <div>
+              <img src="/image/404-computer.svg" alt="" className="w-52" />
+            </div>
+            <div className=" space-y-3">
+              <h3 className="text-2xl font-semibold">{quiz.title}</h3>
+              <p>{quiz.description}</p>
+              <Button>Continue</Button>
+            </div>
           </div>
-          <div className=" space-y-3">
-            <h3 className="text-2xl font-semibold">
-              Lorem, ipsum dolor sit amet consectetur
-            </h3>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem, beatae?
-            </p>
-            <Button>Continue</Button>
-          </div>
-        </div>
-      </Card>
-      <Card className=" hover:scale-[102%] transition-all duration-300">
-        <div className="flex">
-          <div>
-            <img src="/image/404-computer.svg" alt="" className="w-52" />
-          </div>
-          <div className=" space-y-3">
-            <h3 className="text-2xl font-semibold">
-              Lorem, ipsum dolor sit amet consectetur
-            </h3>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem, beatae?
-            </p>
-            <Button>Continue</Button>
-          </div>
-        </div>
-      </Card>
-      <Card className=" hover:scale-[102%] transition-all duration-300">
-        <div className="flex">
-          <div>
-            <img src="/image/404-computer.svg" alt="" className="w-52" />
-          </div>
-          <div className=" space-y-3">
-            <h3 className="text-2xl font-semibold">
-              Lorem, ipsum dolor sit amet consectetur
-            </h3>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem, beatae?
-            </p>
-            <Button>Continue</Button>
-          </div>
-        </div>
-      </Card>
-      <Card className=" hover:scale-[102%] transition-all duration-300">
-        <div className="flex">
-          <div>
-            <img src="/image/404-computer.svg" alt="" className="w-52" />
-          </div>
-          <div className=" space-y-3">
-            <h3 className="text-2xl font-semibold">
-              Lorem, ipsum dolor sit amet consectetur
-            </h3>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem, beatae?
-            </p>
-            <Button>Continue</Button>
-          </div>
-        </div>
-      </Card>
+        </Card>
+      ))}
     </div>
   );
 }
