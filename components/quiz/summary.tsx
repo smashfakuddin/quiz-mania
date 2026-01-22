1ac4ef8088952de4558ffd2cc60d0bb922de7dfa
S.M. Ashfak Uddin <smashfakuddin.anthro.cou@gmail.com>
2026-01-22 19:03:50 +0600

    accuracy data added

--- components/quiz/summary.tsx
+++ components/quiz/summary.tsx
@@ -1,4 +1,4 @@
-import { getLastAttemptedQuiz } from "@/lib/mongo-query";
+import { getAttemptSummary, getLastAttemptedQuiz } from "@/lib/mongo-query";
 import { Card, CardContent, CardHeader } from "../ui/card";
 import { ChartBar } from "./bar-chart";
 import { ChartLine } from "./line-chart";
@@ -7,18 +7,21 @@ import { auth } from "@/auth";
 
 export default async function Summary() {
   const session = await auth();
-  if(!session?.user){
-    return <>not auth</>
+  if (!session?.user) {
+    return <>not auth</>;
   }
   const data = await getLastAttemptedQuiz(session?.user?.id);
+  const summary = await getAttemptSummary(session?.user?.id);
 
-  console.log(data);
+  console.log(summary);
   return (
     <div>
-      <Card >
-        <CardHeader className="text-xl font-semibold">Check Your Performance over the time</CardHeader>
+      <Card>
+        <CardHeader className="text-xl font-semibold">
+          Check Your Performance over the time
+        </CardHeader>
         <CardContent className="flex md:flex-row flex-col gap-5 p-4">
-          <ChartRadial />
+          <ChartRadial total={summary?.accuracy}/>
           <ChartBar />
           <ChartLine />
         </CardContent>
